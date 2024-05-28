4ec8efca6c5d8de799be1ce461a2d84da8689294
VieHiroto <www.nkuc@gmail.com>
2024-05-28 10:25:53 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+    console.log('Script from GitHub is running');
+    // ここに実行したいコードを記述
+    let element = document.querySelector('#exampleId');
+    if (element) {
+        element.click();
+    }
+})();
