cd3412ea336ec4e9e76738c0e9e29bae5897a97c
VieHiroto <www.nkuc@gmail.com>
2024-05-28 10:42:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 (function() {
     'use strict';
     console.log('Script from GitHub is running');
-    // ここに実行したいコードを記述
-    let element = document.querySelector('#exampleId');
-    if (element) {
-        element.click();
-    }
+
+    // すべてのリンクを取得
+    const links = document.querySelectorAll('a');
+
+    // リンクをチェック
+    links.forEach(link => {
+        if (link.href === 'https://www.google.com/') {
+            // メッセージを表示
+            const message = document.createElement('div');
+            message.textContent = 'ここはGoogleのホームです';
+            message.style.position = 'fixed';
+            message.style.top = '10px';
+            message.style.left = '10px';
+            message.style.backgroundColor = 'yellow';
+            message.style.padding = '10px';
+            message.style.border = '1px solid black';
+            document.body.appendChild(message);
+        }
+    });
 })();
